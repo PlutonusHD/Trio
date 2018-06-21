cacf34da7dad11c271812dff153192c4b22d24bb
Joshua Hughes <plutonus@projectplutonus.com>
2018-06-21 19:19:12 +1000

    Woo! The bot now does something!

--- app.js
+++ app.js
@@ -30,6 +30,59 @@ trio.on('ready', () => {
         console.log(chalk.bgRed("Err") + ": " + chalk.red("Trio is not designed to run under a Discord user account!"));
         process.exit(1);
     }
+    console.log(chalk.bgGreen("Trio") + ": Registering commands...");
+    trio.commands = new Discord.Collection();
+    trio.cooldown = new Discord.Collection();
+    const cmds = fs.readdirSync('./data/commands').filter(file => file.endsWith('.js'));
+    let i=0;
+    for (const f of cmds) {
+        const cmd = require(`./data/commands/${f}`);
+        c.commands.set(cmd.name, cmd);
+        i++;
+        console.log(chalk.bgGreen("Trio") + `: Registed command '${cmd.name}'`);
+    }
+    console.log(chalk.bgGreen("Trio") + `: Registered a total of ${i} commands.`);
+
+    // Once complete, do:
+    trio.on('message', msg => {
+        if (!msg.content.startsWith(prefix) || msg.author.bot) return;
+        const args = msg.content.slice(prefix.length).split(/ +/);
+        const commandName = args.shift().toLowerCase();
+        
+        if (!trio.commands.has(commandName)) return;
+        const command = trio.commands.get(commandName);
+        
+        if (!trio.cooldown.has(command.name)) {
+            trio.cooldown.set(command.name, new Discord.Collection());
+        }
+
+        const now = Date.now();
+        const timestamps = trio.cooldown.get(command.name);
+        const cooldownAmount = (command.cooldown * 1000) || 0;
+
+        if (!timestamps.has(msg.author.id)) {
+            timestamps.set(msg.author.id, now);
+            setTimeout(() => timestamps.delete(msg.author.id), cooldownAmount);
+        } else {
+            const expirationTime = timestamps.get(msg.author.id) + cooldownAmount;
+    
+            if (now < expirationTime) {
+                let timeLeft = (expirationTime - now) / 1000;
+                return msg.channel.send({"embed":{"title":"Command Timeout","description":"Please wait "+timeLeft.toFixed(1)+" more second(s) before reusing the `"+command.name+"` command."}});
+            }
+    
+            timestamps.set(msg.author.id, now);
+            setTimeout(() => timestamps.delete(msg.author.id), cooldownAmount);
+        }
+
+        try {
+            command.execute(trio, msg, args);
+        }
+        catch (error) {
+            console.error(error);
+            msg.reply("something went wrong while trying to execute that command!");
+        }
+    });
 });
 
 trio.on('disconnect', event => {
@@ -43,3 +96,5 @@ trio.on('disconnect', event => {
         console.log(chalk.bgRed("Err") + ": " + chalk.red("Disconnected from Discord with code " + event.code));
     }
 });
+
+trio.on
---
+++ data/commands/help.js
@@ -0,0 +1,34 @@
+module.exports = {
+    name: 'help',
+    description: 'List all of my commands or info about a specific command.',
+    cooldown: 3,
+    usage: '[command name]',
+    cooldown: 5,
+    execute(c, msg, args) {
+		const { commands } = msg.client;
+		const { prefix } = require('../config');
+		let out='';
+		let out2='';
+
+		if (!args.length) {
+			out+=commands.map(command => command.name).join('\n');
+			out2+=commands.map(command => command.description).join('\n');
+
+			return msg.channel.send({"embed":{"title":"Commands", "fields":[{"name":"Command","value":out,"inline":true},{"name":"Description","value":out2,"inline":true}]}})
+		}
+
+		const name = args[0].toLowerCase();
+		const command = commands.get(name) || commands.find(c => c.aliases && c.aliases.includes(name));
+
+		if (!command) {
+			return msg.reply('that\'s not a valid command!');
+		}
+
+		out+=`**Command:** ${command.name}\n`
+		
+		if (command.description) out+=`**Description:** ${command.description}\n`;
+		if (command.usage) out+=`**Usage:** ${prefix}${command.name} ${command.usage}\n`;
+
+		msg.channel.send({"embed":{"title": "Command Info", "description": out}});
+	},
+};
